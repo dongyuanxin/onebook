e91c0ee2cac722f5c71504a53044167a77977f08
dongyuanxin <yuanxin.me@gmail.com>
2018-03-09 16:58:30 +0800

    修改配置

--- font/src/router/index.js
+++ font/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/views/Home'
 
 Vue.use(Router)
 
@@ -8,8 +8,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Home
+    },{
+      path:'/home',
+      name:'Home',
+      component: Home
     }
   ]
 })
