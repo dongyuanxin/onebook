bfe40dd22f7372ac36759edd7b0d74c37fccd613
godbmw <xiaochiyijiu@163.com>
2018-03-10 23:33:42 +0800

    增加博文阅读页面

--- font/src/router/index.js
+++ font/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import Blog from '@/views/Blog'
 
 Vue.use(Router)
 
@@ -10,10 +11,16 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Home
-    },{
+    },
+    {
       path:'/home',
       name:'Home',
       component: Home
+    },
+    {
+      path:'/blog',
+      name:"Blog",
+      component:Blog
     }
   ]
 })
