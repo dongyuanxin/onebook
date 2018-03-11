d6823f775cbeb2c10b99e09b0d7af226577a40a8
dongyuanxin <yuanxin.me@gmail.com>
2018-03-11 14:43:06 +0800

    增加遮盖层效果

---
+++ font/src/components/CoverLayer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cover-layer">
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style>
+    .cover-layer{
+        width:100%;
+        height:100%;
+        background-color: black;
+        color: white;
+        opacity: 0.5;
+    }
+</style>
