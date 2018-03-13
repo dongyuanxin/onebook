e39bc9d91ab517072fccbd5de4c91e3e3dbb23d2
godbmw <xiaochiyijiu@163.com>
2018-03-13 21:25:18 +0800

    Chrome内核检查器

---
+++ src/components/PromptBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="prompt-box">
+        <slot name="info"></slot>
+        <slot name="button"></slot>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
+<style>
+    .prompt-box{
+        width:300px;
+        height:200px;
+        padding:5px;
+        border-radius: 10px;
+    }
+</style>
+
