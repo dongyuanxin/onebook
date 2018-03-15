12b169182e87ac3d87131ef50b010b306be75327
godbmw <xiaochiyijiu@163.com>
2018-03-15 15:21:19 +0800

    修复static目录，增加blog下的子路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Blog from '@/views/Blog'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
@@ -21,6 +22,21 @@ export default new Router({
       path:'/blog',
       name:"Blog",
       component:Blog
+    },
+    {
+      path:'/blog/:user/:name', // user : githubID ; name: 仓库名字
+      name:"BlogWithUserName",
+      component:Blog
+    },
+    {
+      path:'/blog/:user', // user : githubID ; 仓库名字默认是 : book
+      name:"BlogWithUser",
+      component:Blog
+    },
+    {
+      path:'*',
+      name:"404",
+      component:Home
     }
   ]
 })
