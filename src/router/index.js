ceba83a7373e3c720d2f3b6b7db68b4e7f0c58e0
dongyuanxin <yuanxin.me@gmail.com>
2018-03-18 17:43:05 +0800

    增加创作页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Blog from '@/views/Blog'
+import Paper from '@/views/Paper'
 
 Vue.use(Router)
 
@@ -32,6 +33,11 @@ export default new Router({
       path:'/book/:user', // user : githubID ; 仓库名字默认是 : book
       name:"BlogWithUser",
       component:Blog
+    },
+    {
+      path:'/paper',
+      name:'Paper',
+      component:Paper
     },
     {
       path:'*',
