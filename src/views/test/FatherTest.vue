853308a81d6bc615529870d50f46df99a5651c70
dongyuanxin <yuanxin.me@gmail.com>
2018-03-23 15:19:21 +0800

    父子组件 beforeUpdate && updated 测试

---
+++ src/views/test/FatherTest.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <test @childchange = "childChange"></test>
+        <div class="test"> {{ msg }} </div>
+    </div>
+</template>
+<script>
+import Test from './Test' 
+export default {
+    data(){
+        return {
+            msg: 'I am father'
+        }
+    },
+    components:{
+        Test
+    },
+    methods:{
+        childChange(args){
+            console.log("Child : "+ args)
+            this.msg = args
+        }
+    },
+    updated(){
+        console.log('Father updated')
+    }
+}
+</script>
