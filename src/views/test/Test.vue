7f9a858af6e08c8182a8ba62fdb322f7a86589ba
godbmw <xiaochiyijiu@163.com>
2018-03-23 15:19:21 +0800

    父子组件 beforeUpdate && updated 测试

---
+++ src/views/test/Test.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="test" @click="changeMsg">
+        <!-- {{ msg }} -->
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg:'test'
+        }
+    },
+    beforeMount(){
+        console.log("before mount")
+        // alert('debug `before mount`')
+    },
+    mounted(){
+        console.log('mounted')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate')
+    },
+    updated(){
+        console.log('updated')
+    },
+    methods:{
+        changeMsg(){
+            this.msg = this.msg === 'test' ? 'ok' : 'test'
+            this.$emit('childchange','From children: ' + this.msg)
+        }
+    }
+}
+</script>
+<style>
+    .test{
+        height:100px;
+        width:100px;
+        cursor: pointer;
+        margin:0 auto;
+        background-color:mistyrose;
+        border-radius: 5px;
+    }
+</style>
+
