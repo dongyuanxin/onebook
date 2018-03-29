cc4f9c80e6c2c8c7cc5c239e6e490fd5e69123b9
godbmw <xiaochiyijiu@163.com>
2018-03-29 14:49:52 +0800

    修改首页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,151 +1,139 @@
 <template>
-  <div class="home" ref="home">
-    <navigation :style="{opacity:navOpacity}"></navigation>
-      <vue-particles
-        class='particles'
-        :style="{opacity:particleOpacity}"
-        color="#cccccc" 
-        :clickEffect="true"
-        shapeType = "polygon"
-        :particlesNumber = "100"
-        :linesDistance = "130"
-      ></vue-particles>
-    <div class="hero" v-for="(item,index) in msg"  :class="getHeroClass(index)">
-      <span @mouseenter="particleOpacity=0" @mouseleave="particleOpacity = 1" @click="jumpTo(item.href)"> {{ item.info }} </span>
+    <div>
+        <navigation></navigation>
+        <div class="dev-home" 
+        v-for=" (item , index) in imgSrcArr "
+        :style=" { backgroundImage : 'url('+item+')' , opacity : bgOpacity( index )} ">
+        </div>
+        <div class="dev-intro">
+            <div class="dev-intro--title" 
+            @click="jumpTo( href )">
+                {{ intro.title }}
+            </div>
+            <div class="dev-intro--sentence">
+                {{ intro.sentence }}
+            </div>
+        </div>
     </div>
-    <my-footer></my-footer>
-  </div>
 </template>
-
 <script>
 import Navigation from '@/components/Navigation.vue'
-import MyFooter from '@/views/Footer'
+
 export default {
-  name: 'Home',
-  data () {
-    return {
-      msg: [
-        {
-          info:'I\'m GODBMW',
-          href:'/book'
-        },
-        {
-          info:'Read Book',
-          href:'/book'
-        },
-        {
-          info:'Write Now',
-          href:'/paper'
+    data(){
+        return {
+            imgSrcArr : [
+                '/static/images/background/1.jpg',
+                '/static/images/background/2.jpg',
+                '/static/images/background/3.jpg'
+            ],
+            introArray : [
+                {
+                    title : 'onebook',
+                    sentence : 'open-source platform',
+                    href : '/'
+                },
+                {
+                    title : 'book',
+                    sentence : 'get your own book',
+                    href : '/'
+                },
+                {
+                    title : 'write',
+                    sentence : 'write whatever want',
+                    href : '/'
+                }
+            ],
+            scanNow : 0 ,
+            interval : 4000 
         }
-      ],
-      index:0,
-      scrollTop:{
-        last:0,
-        now:0,
-        ready:true
-      },
-      navOpacity:0,
-      showParticals:true,
-      particleOpacity:1
-    }
-  },
-  components:{
-    Navigation,
-    MyFooter
-  },
-  methods:{
-    getHeroClass(index){
-      return index%2===0?'plural-hero':'odd-hero'
     },
-    handleScroll:function(event){
-      this.flushNav()
-      if(this.scrollTop.ready) {
-        this.scrollTop.now = document.documentElement.scrollTop
-        let direction = this.scrollTop.now>this.scrollTop.last ?'down' :'up'
-        // 缓存机制，防止浏览器卡死
-        this.scrollTop.ready = false
-        // this.moveTo(direction) // bug
+    mounted(){
         let that = this
-        setTimeout(function(){
-          // 获取上次的滚动后的scrollTop，不能放在setTimeout外面，因为有延迟
-          that.scrollTop.last = document.documentElement.scrollTop
-          that.scrollTop.ready=true
-        },300)
-      }
+        setInterval( function() {
+            that.scanNow = ( that.scanNow + 1 ) % that.imgSrcArr.length ;
+        } , that.interval )
     },
-    flushNav(){
-      let { scrollTop,clientHeight } = document.documentElement
-      this.navOpacity = (scrollTop + 10) > clientHeight ? 1 : 0;
+    computed:{
+        intro(){
+            return this.introArray[ this.scanNow ]
+        },
+        href(){
+            return this.introArray[ this.scanNow ].href
+        }
     },
-    moveTo(direction){
-      let { scrollHeight ,scrollTop,clientHeight } = document.documentElement
-      console.log(scrollHeight + ' ' +  clientHeight + ' '+ scrollTop)
-      if(direction==='down') {
-        document.documentElement.scrollTop = 1234
-      }
+    methods:{
+        bgOpacity( index ){
+            return index === this.scanNow ? '1.0' : '0' ; 
+        },
+        introMargin( index ) {
+            return index === this.scanNow ? '0px' : '100%';
+        },
+        toggleBg(){
+            this.scanNow = ( this.scanNow + 1 ) % this.imgSrcArr.length ;
+        },
+        jumpTo(href){
+            this.$router.push( href )
+        }
     },
-    jumpTo(href){
-      this.$router.push(href)
+    components : {
+        Navigation
     }
-  },
-  mounted(){
-    window.addEventListener('scroll',this.handleScroll)
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.hero {
-  /* 实现 hero 占满屏幕，并且不出现 scroll*/
-  margin-top:0px !important; 
-  height: 100vh;
-  width: 100%;
-  /* *** */
-  position: relative;
-  text-align: center;
-  margin-top:10px;
-  font-family: "Montserrat", sans-serif;
-}
-
-/* 单数和复数位置的页面 */
-.odd-hero {
-  background-color: #bbbbc4;
-  color: #282931;
-}
-.odd-hero span{
-  border-bottom: 2px solid #282931;
-}
-.plural-hero{
-  background-color: #282931;
-  color: #bbbbc4;
-}
-.plural-hero span{
-  border-bottom: 2px solid #bbbbc4;
-}
-/*****/
-
-.hero span{
-  cursor: pointer;
-  font-size: 50px;
-  font-weight: 500;
-  letter-spacing: 2px;
-  padding-bottom: 2px;
-  /* 实现span标签剧中效果 */
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%,-50%);
-  /* *** */
-  z-index: 200;
-}
-
-.particles {
-  position: fixed;
-  left:0;
-  right: 0;
-  z-index: 100;
-  height: 100vh;
-  transition: all .5s linear;
-}
+    @keyframes toggler{
+        0% {
+            margin-left: 100%;
+            opacity: 1.0;
+        }
+        50% {
+            margin-left: 0px;
+            opacity: 1.0;
+        }
+        90% {
+            margin-left: 0px;
+            opacity: 1.0;
+        }
+        99%{
+            margin-left: 0px;
+            opacity: 0.0;
+        }
+        100%{
+            margin-left: 100%;
+            opacity: 0;
+        }
+    }
+    .dev-home {
+        width: 100%;
+        height: 100vh;
+        background-size: 100% 100vh;
+        background-repeat: no-repeat;
+        transition : opacity 0.5s linear;
+        position: fixed;
+        /* filter: blur(2px); */
+    }
+    .dev-intro {
+        width:100%;
+        position: fixed;
+        z-index: 300;
+        top : 50%;
+        left: 50%;
+        text-align: center;
+        color: white;
+        font-size: 100px;
+    }
+    .dev-intro {
+        transition: all 1s linear;
+        transform: translate(-50% , -70%);
+        animation: toggler 4s linear infinite; 
+    }
+    .dev-intro--title {
+        font-size : 1em;
+        cursor: pointer;
+    }
+    .dev-intro--sentence {
+        font-size: 0.5em;
+    }
 </style>
