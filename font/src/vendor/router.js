efe95ce2544f6e21bc6e61c19d33ca2cdefb5bfd
dongyuanxin <yuanxin.me@gmail.com>
2018-06-11 18:12:45 +0800

    实现:项目路由配置

--- font/src/vendor/router.js
+++ font/src/vendor/router.js
@@ -3,20 +3,40 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
+const Book = () => import("@/views/Book/Index");
+const User = {
+  Login: () => import("@/views/User/Login"),
+  Space: () => import("@/views/User/Space")
+};
 const _Error = () => import("@/views/Error");
 const Test = () => import("@/views/Test");
 
 const router = new Router({
   routes: [
     {
-      path: "*",
-      name: "error",
-      component: _Error
+      path: "/book/:platform/:user/:depository",
+      name: "book",
+      component: Book
+    },
+    {
+      path: "/user/login",
+      name: "login",
+      component: User.Login
+    },
+    {
+      path: "/user/space",
+      name: "space",
+      component: User.Space
     },
     {
       path: "/test",
       name: "test",
       component: Test
+    },
+    {
+      path: "*",
+      name: "error",
+      component: _Error
     }
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ font/src/vendor/setting.js
@@ -0,0 +1,9 @@
+const git = {
+  github: "https://github.com"
+};
+
+const server = {
+  url: "//193.112.241.232"
+};
+
+export { git, server };
