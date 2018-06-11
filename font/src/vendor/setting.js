00e8c5e42a293ab279f5b1718f04c81d46a3d12d
dongyuanxin <yuanxin.me@gmail.com>
2018-06-11 21:08:58 +0800

    配置:markdown解析库和axios

--- font/src/vendor/setting.js
+++ font/src/vendor/setting.js
@@ -2,8 +2,10 @@ const git = {
   github: "https://github.com"
 };
 
+const configFile = "book.json";
+
 const server = {
   url: "//193.112.241.232"
 };
 
-export { git, server };
+export { git, server, configFile };
