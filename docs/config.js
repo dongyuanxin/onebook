30953796d69e660e6a5696157f6126c9f0041081
dongyuanxin <yuanxin.me@gmail.com>
2018-06-23 20:57:27 +0800

    修改: v2.0版本中文文档

--- docs/config.js
+++ docs/config.js
@@ -6,7 +6,7 @@ const langs = [
 myNav = {
     default: [
         { 
-            title: 'Home', path: '/home' // source: /path/to/thefile.md or source: /url/to/thefile.md, can be github file
+            title: 'Home', path: '/home'
         },
         { 
             title: 'FAQ', path: '/question' 
@@ -37,47 +37,15 @@ myNav = {
 docute.isDev = false;
 
 docute.init({
-    // root directory
     url: docute.isDev? '.' : 'http://193.112.241.232:8000',
-    // mode of route
     routeMode: 'history', // or be 'hash'
     landing: {
-        // source
         source: 'landing.html',
-        // inline markdown
-        // markdown: '# The landing page using inline markdown',
-        // inline html
-        // html: '<h2>The landing page using inline html</h2>',
-        // or vue component
     },
-
-    // config sidebar
-    // sidebar: false, // hide the sidebar
-    // disableSidebarToggle: true, // disable the sidebar toggle
-    // toc of the sidebar
-    // tocVisibleDepth: 3, // show the toc of level 3 
-    // custom toc is also ok
-
-
-    // nav: langs,
     nav: myNav,
-    // config icons in the navigate bar
-    // github twitter edit menu link search close info
-    // custom icon is also ok
     icons: [{
         icon: 'github',
         label: 'Contribute on GitHub',
         link: 'https://github.com/dongyuanxin/onebook'
     }]
-
-    // markdown setting: see the docs of marked
-    /*
-    marked: {
-        smartypants: true
-        // ...
-    }
-    */
-   
-    // tools for debug
-    // debug: true
 });
--- docs/zh-hans/contact.md
+++ docs/zh-hans/contact.md
@@ -3,6 +3,6 @@ nav: zh-Hans
 ---
 
 # 联系我们
-* 如果你发现了bug，欢迎给我们提出[issue](https://github.com/godbmw/book/issues)
-* 你还可以给我们发送一封[email](godbmw.com@gmail.com)
-* 或者可以直接查看我们的[项目地址](https://github.com/godbmw/onebook)，如果觉得项目不错，请给我们一个star
+* 如果你发现了bug，欢迎给我们提出[issue](https://github.com/dongyuanxin/onebook/issues)
+* 你还可以给我们发送一封[email](yuanxin.me@gmail.com)
+* 或者可以直接查看我们的[项目地址](https://github.com/dongyuanxin/onebook)，如果觉得项目不错，请给我们一个star
